159dfc20948c7177ba4040c0c843d9735faebd4f
sbeimel <sbeimel@web.de>
2026-03-18 12:46:59 +0100

    Update 0020_m3uaccount_proxy.py

--- apps/m3u/migrations/0020_m3uaccount_proxy.py
+++ apps/m3u/migrations/0020_m3uaccount_proxy.py
@@ -3,6 +3,22 @@
 from django.db import migrations, models
 
 
+def add_proxy_field_if_not_exists(apps, schema_editor):
+    """Add proxy field only if it doesn't exist"""
+    from django.db import connection
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT column_name 
+            FROM information_schema.columns 
+            WHERE table_name='m3u_m3uaccount' AND column_name='proxy'
+        """)
+        if not cursor.fetchone():
+            cursor.execute("""
+                ALTER TABLE m3u_m3uaccount 
+                ADD COLUMN proxy varchar(255) NULL
+            """)
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -10,14 +26,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='m3uaccount',
-            name='proxy',
-            field=models.CharField(
-                blank=True,
-                help_text='HTTP proxy URL (e.g., http://proxy.example.com:8080) for this M3U account',
-                max_length=255,
-                null=True
-            ),
-        ),
+        migrations.RunPython(add_proxy_field_if_not_exists, migrations.RunPython.noop),
     ]
